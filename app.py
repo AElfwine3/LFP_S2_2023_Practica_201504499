f39b30948686ebd2a8645d5ad397c5325a86c44b
Elvin Mayen <2534709860101@ingenieria.usac.edu.gt>
2023-08-22 22:20:46 -0600

    Funciones principales

--- app.py
+++ app.py
@@ -1,28 +1,40 @@
-print("- - - - - - - - - - - - - - - - - - - - - - - -")
-print("Practica 1 Lenguajes Formales y de Programacion")
-print("- - - - - - - - - - - - - - - - - - - - - - - -")
-while True:
-    print()
-    print("Elige una opcion por favor:")
-    print()
-    print("1. Cargar inventario inicial")
-    print("2. Cargar instrucciones de movimientos")
-    print("3. Crear informe de inventario")
-    print("4. Salir")
-    print()
+import carga_archivos
 
-    opcion = input("Ingresa una opcion: ")
-    print()
-
-    if opcion == "1":
-        print("1. Cargar inventario inicial")
+def menu():
+    print("- - - - - - - - - - - - - - - - - - - - - - - -")
+    print("Practica 1 Lenguajes Formales y de Programacion")
+    print("- - - - - - - - - - - - - - - - - - - - - - - -")
+    while True:
         print()
-    elif opcion == "2":
+        print("Elige una opcion por favor:")
+        print()
+        print("1. Cargar inventario inicial")
         print("2. Cargar instrucciones de movimientos")
-    elif opcion == "3":
         print("3. Crear informe de inventario")
-    elif opcion == "4":
         print("4. Salir")
-        break
-    else:
-        print("Por favor elige una opcion del menu.")
+        print()
+
+        opcion = input("Ingresa una opcion: ")
+        print()
+
+        if opcion == "1":
+            print("Cargar Inventario Inicial")
+            carga_archivos.carga_inventario()
+            print()
+        elif opcion == "2":
+            print("Cargar instrucciones de movimientos")
+            carga_archivos.carga_movimientos()
+            print()
+        elif opcion == "3":
+            print("Crear informe de inventario")
+            carga_archivos.reporte_inventario()
+            print()
+        elif opcion == "4":
+            print("4. Salir")
+            break
+        else:
+            print("Por favor elige una opcion del menu.")
+
+
+if __name__ == "__main__":
+    menu()
---
+++ carga_archivos.py
@@ -0,0 +1,81 @@
+import tkinter as tk
+from tkinter import filedialog
+from tabulate import tabulate
+import os
+
+productos = []
+
+def agregar_stock(nombre, cantidad, bodega):
+    for producto in productos:
+        if producto['nombre'] == nombre:
+            if producto['bodega'] == bodega:
+                producto['cantidad'] += cantidad
+                return
+            else:
+                print(f'El producto {producto["nombre"]} no existe en bodega {producto["bodega"]}')
+                return
+
+def vender_producto(nombre, cantidad, bodega):
+    for producto in productos:
+        if producto['nombre'] == nombre:
+            if producto['bodega'] == bodega:
+                if producto['cantidad'] < cantidad:
+                    print(f'No hay suficiente stock de {nombre} en {bodega}')
+                    return
+                else:
+                    producto['cantidad'] -= cantidad
+                    return
+
+def carga_inventario():
+    root = tk.Tk()
+    root.withdraw()
+    file_path = filedialog.askopenfilename()
+
+    if not file_path.endswith('.inv'):
+        print('No es un archivo valido')
+    else:
+        with open(file_path, 'r') as file:
+            for line in file:
+                datos = [dato.strip() for dato in line.rstrip().split(';')]
+                producto = {
+                    'nombre': datos[0].split()[1],
+                    'cantidad': int(datos[1]),
+                    'precio': datos[2],
+                    'bodega': datos[3]
+                }
+                if datos[0].split()[0] == 'crear_producto':
+                    productos.append(producto)
+    print('Inventario cargado con exito!!!')
+
+
+def carga_movimientos():
+    root = tk.Tk()
+    root.withdraw()
+    file_path = filedialog.askopenfilename()
+
+    if not file_path.endswith('.mov'):
+        print('No es un archivo valido')
+    else:
+        with open(file_path, 'r') as file:
+            for line in file:
+                datos = [dato.strip() for dato in line.rstrip().split(';')]
+                movimiento = {
+                    'nombre': datos[0].split()[1],
+                    'cantidad': int(datos[1]),
+                    'bodega': datos[2]
+                }
+                if datos[0].split()[0] == 'agregar_stock':
+                    agregar_stock(movimiento['nombre'], movimiento['cantidad'], movimiento['bodega'])
+                elif datos[0].split()[0] == 'vender_producto':
+                    vender_producto(movimiento['nombre'], movimiento['cantidad'], movimiento['bodega'])
+    print('Movimientos realizados con exito!!!')
+
+def reporte_inventario():
+    tabla = []
+    for producto in productos:
+        tabla.append([producto['nombre'], producto['cantidad'], producto['precio'], producto['cantidad'] * float(producto['precio']), producto['bodega']])
+    
+    with open('reporte.txt', 'w') as file:
+        file.write(tabulate(tabla, headers=['Producto', 'Cantidad', 'Precio Unitario', 'Valor Total', 'Ubicacion']))
+    
+    os.system('start reporte.txt')
